d8c60d22dd76e847084192550170c9fb09f84074
piedrohammer <piiedrohammer@gmail.com>
2021-12-07 13:16:14 -0300

    Initial commit

---
+++ src/main/java/exercicios/NomeEIdade.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+public class NomeEIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+
+        while(true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("Idade: ");
+        }
+
+        System.out.println("Continua aqui...");
+
+    }
+
+}
